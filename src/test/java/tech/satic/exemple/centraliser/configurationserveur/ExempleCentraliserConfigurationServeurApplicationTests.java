4af9b0a9fdc05422c78c815519e7f9dffb2ad89e
lorincet <lorincet@gmail.com>
2021-12-28 15:44:14 +0100

    Initial commit

---
+++ src/test/java/tech/satic/exemple/centraliser/configurationserveur/ExempleCentraliserConfigurationServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.satic.exemple.centraliser.configurationserveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExempleCentraliserConfigurationServeurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
